e5ee841623499d805903c28fec8b55847c1162c4
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-04-15 11:24:04 +0200

    + Natuarl logarithm

--- natural-logarithm.py
+++ natural-logarithm.py
@@ -6,11 +6,9 @@ y = np.log(x)
 
 plt.plot(x, y, label='log(x)')
 
-
 def mark_coord(x, label, offset):
     y = np.log(x)
     plt.scatter(x, y, color='red')
-#   plt.annotate(label, (x, y), textcoords="offset points", xytext=( 14,-10), ha='center', fontsize=8, color='red')
     plt.annotate(label, (x, y), textcoords="offset points", xytext=offset   , ha='center', fontsize=8, color='red')
 
 
@@ -19,21 +17,9 @@ mark_coord(  1       , 'log(1)'  , ( 14,-10))
 mark_coord(  np.e    , 'log(e)'  , (-10,  6))
 mark_coord(  np.e**2 , 'log(e^2)', (-10,-11))
 
-# Mark special points …
-# plt.scatter(   1   , np.log(1      ), color='red')
-# plt.scatter(np.e   , np.log(np.e   ), color='red')
-# plt.scatter(np.e**2, np.log(np.e**2), color='red')
-# 
-# # … and annotate them
-# plt.annotate('log(1/e)', (1/np.e  , np.log(1/np.e  )), textcoords="offset points", xytext=( 14,-10), ha='center', fontsize=8, color='red')
-# plt.annotate('log(1)'  , (   1    , np.log(1       )), textcoords="offset points", xytext=( 14,-10), ha='center', fontsize=8, color='red')
-# plt.annotate('log(e^1)', (np.e    , np.log(np.e    )), textcoords="offset points", xytext=(-10,  6), ha='center', fontsize=8, color='red')
-# plt.annotate('log(e^2)', (np.e**2 , np.log(np.e**2 )), textcoords="offset points", xytext=(-10,-11), ha='center', fontsize=8, color='red')
-
 plt.xlabel('x')
 plt.ylabel('log(x)')
 plt.title('Natuaral logarithm')
 
 plt.grid()
-# plt.show()
 plt.savefig('img/natural-logarithm.png')
